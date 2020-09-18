ba592e68b155e2a02cd9e29f497d19a331e835c6
unknown <leedanleo.91@gmail.com>
2020-09-17 23:04:59 -0700

    Updates

---
+++ precache-manifest.eb3e0e24b52e799edb2f4aa541450e9b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5d97ff37a3ffd86cb9861e0dddd8e0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8b77edbfe7043944197f",
+    "url": "/portfolio/static/css/main.0380e23e.chunk.css"
+  },
+  {
+    "revision": "2273d0467f00add971e9",
+    "url": "/portfolio/static/js/2.1b4dd0e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1b4dd0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b77edbfe7043944197f",
+    "url": "/portfolio/static/js/main.37213da0.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "2c93e5f33e1d3f56ee0a76e98265d8a2",
+    "url": "/portfolio/static/media/GitHub.2c93e5f3.png"
+  },
+  {
+    "revision": "14321b91944f6aba1f15b42e7e484678",
+    "url": "/portfolio/static/media/OIT.14321b91.svg"
+  },
+  {
+    "revision": "273ddaf5b2e2aaa1d4501b0db069a6a7",
+    "url": "/portfolio/static/media/PSU.273ddaf5.svg"
+  },
+  {
+    "revision": "39b839932ac14b711bac4b66f971a47a",
+    "url": "/portfolio/static/media/activities.39b83993.png"
+  },
+  {
+    "revision": "2ce543b7baeab5aca82df866ab84dee3",
+    "url": "/portfolio/static/media/art1.2ce543b7.jpg"
+  },
+  {
+    "revision": "fd2669d7681b3880c2795f6218b6a70f",
+    "url": "/portfolio/static/media/art2.fd2669d7.jpg"
+  },
+  {
+    "revision": "c83b42159e6ab94a8d5ea56033e50c5e",
+    "url": "/portfolio/static/media/art3.c83b4215.jpg"
+  },
+  {
+    "revision": "ce1bd2fdf83825f625840fda0db76ba9",
+    "url": "/portfolio/static/media/daybeach1.ce1bd2fd.mp4"
+  },
+  {
+    "revision": "7da4d754998d5e1949ead43d125dc131",
+    "url": "/portfolio/static/media/daybeach1static.7da4d754.PNG"
+  },
+  {
+    "revision": "dbc6ed93a99bdd136944399043f4d43b",
+    "url": "/portfolio/static/media/mail.dbc6ed93.png"
+  },
+  {
+    "revision": "a71bc41f9c13b68d8ef521c89d0864e9",
+    "url": "/portfolio/static/media/paint.a71bc41f.png"
+  },
+  {
+    "revision": "18bc71a00397869c61b802eaf528c41d",
+    "url": "/portfolio/static/media/profile.18bc71a0.png"
+  },
+  {
+    "revision": "9d8d441923ddcb9f86cb5e5707594dce",
+    "url": "/portfolio/static/media/waterfall1.9d8d4419.mp4"
+  }
+]);
